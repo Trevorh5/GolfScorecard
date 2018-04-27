7db387b2565503770f85986a0d0f43e88d255b9f
trevor <trevor.hallett.5@gmail.com>
2018-04-26 21:44:22 -0600

    fifth commit, you can now add and delete users and there is much more advanced css now.

--- action.js
+++ action.js
@@ -4,7 +4,6 @@ let allcourses;
 let selTee;
 let selcourse;
 let holes = 18;
-let maxplayers = 4;
 
 
 
@@ -24,17 +23,7 @@ function loadDoc() {
     xhttp.send();
 }
 
-function getPlayers(){
 
-    if(numplayers < 4) {
-        numplayers++;
-    }else{
-        $('#maxPlayers').text('Max 4 Players');
-    }
-
-    fillCard();
-
-}
 
 
 function getCourse(id){
@@ -53,50 +42,96 @@ function getCourse(id){
     };
     xhttp.open("GET", "https://uxcobra.com/golfapi/course"+ id +".txt", true);
     xhttp.send();
+
 }
 
-function setTee(teeindex){
+
+
+
+function buildCard(teeindex){
     $('.right').html('');
     selTee = teeindex;
-    let mycourse = selcourse.data.holes;
-    for(let i = 0; i < mycourse.length; i++){
-        $('.right').append("<div class='column' id='c"+ i +"'>"+
-            "<div class='cheader'>"+ (i+1) +"</div>" +
+
+    let totalpar = 0;
+    let totalyards = 0;
+
+    for(t = 0; t < selcourse.data.holes.length; t++){
+
+        totalpar += selcourse.data.holes[t].teeBoxes[selTee].par;
+        totalyards += selcourse.data.holes[t].teeBoxes[selTee].yards;
+    }
+
+
+
+    for(let i = 0; i < selcourse.data.holes.length; i++) {
+        $('.right').append("<div class='column' id='c" + i + "'>" +
+            "<div class='cheader'>" + (i + 1) + "</div>" +
             "<div class='hcp'>HDCP:" + selcourse.data.holes[i].teeBoxes[selTee].hcp + "</div>" +
             "<div class='yards'>" + selcourse.data.holes[i].teeBoxes[selTee].yards + " yds</div>" +
+            "<div class='pars'>Par: " + selcourse.data.holes[i].teeBoxes[teeindex].par + "</div>" +
             "<div class='scoreboxes'></div>" +
-            "</div>");
+            "</div>"
+        );
     }
-    
+
+    let inScore = "";
+    let outScore = "";
+    let totalScore = "";
+
+
+    $('.right').append("<div id='totalsCol'>" +
+        "<div class='theader'>Total</div>" +
+        "<div class='empty'></div>" +
+        "<div class='totYards'>" + totalyards + "</div>" +
+        "<div class='totPar'>" + totalpar + "</div>" +
+        "</div>"
+    );
 
 }
 //rows are horizontal
+function addPlayer(){
 
+    if(numplayers < 4) {
+        numplayers++;
+    }else{
+        $('#maxPlayers').show();
+    }
+
+    fillCard();
+
+}
 
 function fillCard(){
     $(".names").text('');
     $(".scoreboxes").html('');
     for(let p = 1; p <= numplayers; p++){
-        $('.names').append("<div contenteditable='true' class='playerlabel'> player "+ p +"</div>");
+        $('.names').append("<div contenteditable='true' class='playerlabel' id='playlabel" + p + "'> Player "+ p +
+            "<span id='deletePlayer' class='far fa-trash-alt' onclick='removePlayer(" + p + ")'></span> </div>");
 
         $('.holeinput').val('');
-        for(let h = 0; h < selcourse.data.holes.length; h++){
-            $("#c" + h).children('.scoreboxes').append("<input onkeyup='calcScore(" + p + ")' class='holeinput' id='p"+ p + "h" + h + "' type='number'>");
+        for(let h = 1; h <= selcourse.data.holes.length; h++){
+            $("#c" + h).children('.scoreboxes').append("<input onkeyup='getPar(" + p + ")' class='holeinput' id='p"+ p + "h" + h + "' type='number'>");
         }
     }
-    //getTotalColumns();
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-function getTotalColumns(){
 
-}
-
-function calcScore(playerNum){
-    let totalScore;
-    for(let t = 1; t <= holes; t++){
-        totalScore = $("p" + playerNum + "h" + t).val();
+function getPar(player){
+    let score = 0;
+    for (let s = 1; s <= holes; s++){
+        score += $("#p" + player + "h" + s).val();
     }
+}
 
+
+function removePlayer(id){
+    $("#playlabel" + id).remove();
 
 }
+
+// function calcScore(playerNum){ <---might Get Replaced by getPar()---->
+//     let totalScore = 0;
+//     for(let t = 1; t <= holes; t++){
+//         totalScore += $("p" + playerNum + "h" + t).val();
+//     }
+// }
